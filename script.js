a6d3abbca4ce413e530713087ee8782e16cd5bc2
Alyssa Petersen <aptrsn11@gmail.com>
2022-07-31 15:36:50 -0400

    add intiital window prompts

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //holds user data
-function userResponse(){
-//prompts and confirms
+function userResponse() {
+  //prompts and confirms
+  var passwordLength = window.prompt(
+    "How long do you want your password to be? Choose between 8-128 characters.",
+    8
+  );
+
+  var useLowerCaseCharacters = window.confirm(
+    "Do you want lowercase letters in your password?"
+  );
+
+  var useUpperCaseCharacters = window.confirm(
+    "Do you want uppercase letters in your password?"
+  );
+
+  var useNumbers = window.confirm("Do you want numbers in your password?");
+
+  var useSpecialCharacters = window.confirm(
+    "Do you want special characters in your password?"
+  );
+
+  return {
+    length: passwordLength,
+    lowercase: useLowerCaseCharacters,
+    uppercase: useUpperCaseCharacters,
+    numbers: useNumbers,
+    special: useSpecialCharacters,
+  };
 }
+
 //generates user password based upon data that is passed
-function generatePassword(){
-var promptData = userResponse();
+function generatePassword() {
+  var promptData = userResponse();
+  console.log(promptData);
+  return "password";
 }
 
 // Write password to the #password input
@@ -15,7 +44,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
